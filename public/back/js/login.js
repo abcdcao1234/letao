c479a9cc96397c32018cb1e49ab72fe90076dd26
cyr <1756219582@qq.com>
2018-04-08 01:42:16 +0800

    校验功能完成一般

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,5 +1,6 @@
 //入口函数可以避免全局污染
 $(function(){
+    //1.表单校验
     $('#form').bootstrapValidator({
       //2. 指定校验时的图标显示，默认是bootstrap风格
       feedbackIcons: {
@@ -8,6 +9,7 @@ $(function(){
         validating: 'glyphicon glyphicon-refresh'
       },
       fields:{
+           //用户名校验
            username:{
              validators:{
                notEmpty:{
@@ -17,23 +19,60 @@ $(function(){
                  min:2,
                  max:6,
                  message:"用户名必须在2-6位之间"
+               },
+               callback:{
+                 message:'用户不存在'
                }
              }
 
            },
+           //密码校验
            password:{
               validators:{
                  notEmpty:{
                     message:'密码不能为空',
                  },
+                //长度校验
                 stringLength:{
                    min:6,
                    max:12,
                    message:'密码必须在6-12位之间'
+                },
+                callback:{
+                   message:'密码错误'
                 }
               }
            },
 
       },
     });
+   //  2.登录请求
+  $('#form').on('success.form.bv',function(e){
+    e.preventDefault();
+    $.ajax({
+        url:'/employee/employeeLogin',
+        type:'post',
+        dataType:'json',
+        data:$('#form').serialize(),
+        success:function( info ){
+           if( info.success){
+              alert('登录成功');
+           }
+          if(info.error==1000){
+            $('#form').data('bootstrapValidator').updateStatus('username',INVALID,"callback")
+          }
+          if(info.error=1001){
+              //alert('密码错误');
+            //参数1：字段名称，参数二:校验状态，参数三：检验规则，可以设置检验文本
+            $('#form').data('bootstrapValidator').updateStatus('password',INVALID,"callback")
+          }
+        }
+
+    })
+  })
+//  表单重置功能的实现
+  $('[type="reset"]').on('click',function(){
+    $('#form').data('bootstrapValidator').resetForm();
+  })
+
 })
